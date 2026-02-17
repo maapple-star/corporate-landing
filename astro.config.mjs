4e998728caa3a79cd7062bc670f2b69bb2d7e807
Suhel Makkad <suhel.makkad@tamara.co>
2026-02-17 19:48:34 +0400

    Add deploy workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://maapple-star.github.io",
+  base: "/corporate-landing",
   vite: {
     plugins: [tailwindcss()],
   },
